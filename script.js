3dd9a6e0feb7108a3273d5c5fa355b660e788249
thiagoanizio <thiagomi1912@gmail.com>
2024-03-19 11:44:51 -0300

    Menu lateral com alterações de links e opções a fazer

---
+++ script.js
@@ -0,0 +1,4 @@
+const sidebarBtn = document.querySelector(".toggle-btn");
+const sidebar = document.querySelector("aside");
+
+sidebarBtn.addEventListener("click", () => {document.body.classList.toggle("active");});
