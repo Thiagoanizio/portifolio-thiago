e7716ec5f89b268ae035d59be83a7eaaadb0bab3
Thiagoanizio <thiagomi1912@gmail.com>
2024-03-20 21:18:27 -0300

    portifólio concluido, indo para detalhes de responsividade e aualização de conteúdo

--- script.js
+++
@@ -1,4 +0,0 @@
-const sidebarBtn = document.querySelector(".toggle-btn");
-const sidebar = document.querySelector("aside");
-
-sidebarBtn.addEventListener("click", () => {document.body.classList.toggle("active");});
